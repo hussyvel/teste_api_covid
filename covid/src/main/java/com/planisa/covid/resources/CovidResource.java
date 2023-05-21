c1bce991e8507156ef68be3d24087534ef60426e
Hussyvel Ribeiro <hussyribeiro@hotmail.com>
2023-05-21 16:10:04 -0300

    atualizando a aplicação

--- covid/src/main/java/com/planisa/covid/resources/CovidResource.java
+++ covid/src/main/java/com/planisa/covid/resources/CovidResource.java
@@ -1,8 +1,10 @@
 package com.planisa.covid.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,38 +28,56 @@ import io.swagger.annotations.ApiOperation;
 @RequestMapping(value="/api") //URI padrão
 @Api(value="API REST Covid")
 @CrossOrigin(origins="*") //libera todos os domínios da api
-public class CovidResource {
 
+public class CovidResource {
+	
+	
 	@Autowired //conectar com os métodos 
 	CovidRepository covidRepository;
 	
-	@GetMapping("/covid")
+	@GetMapping("/covids/get_covids")
 	@ApiOperation(value="Retorna uma lista com os Estados com cases, deaths")
 	public List<Covid> listaStates(){
 		return covidRepository.findAll();
 	}
 	
-	@GetMapping("/covid/{id}")
+	@GetMapping("/covids/covid_id/{id}")
 	@ApiOperation(value="Retorna um Estado com cases, deaths")
 	public Covid listaStateUnico(@PathVariable(value="id") long id){
 		return covidRepository.findById(id);
 	}
 	
-	@PostMapping("/covid")
+
+	@PostMapping("/covids/get_covids")
+	public ResponseEntity<Covid> criarCovid(@RequestBody Covid covid) {
+		covid.setState("");
+        Covid novoCovid = covidRepository.save(covid);
+        return ResponseEntity.created(URI.create("/post_covids/" + novoCovid.getId()))
+                .body(novoCovid);
+    }
+	
+	
+/*
+	@PostMapping("/post_covid")
 	@ApiOperation(value="Salva um Estado")
 	public Covid salvarCovid(@RequestBody Covid covid) {
 		return covidRepository.save(covid);
 	}
+*/
 	
-	@DeleteMapping("/covid")
+
+		
+	@DeleteMapping("covids/delete_covid")
 	@ApiOperation(value="Delete um Estado com cases, deaths")
 	public void deletaCovid (@RequestBody Covid covid) {
 		covidRepository.delete(covid);
 	}
 	
-	@PutMapping("/covid")
+	@PutMapping("covids/update_covid")
 	@ApiOperation(value="Atualiza um Estado com cases, deaths")
 	public Covid atualizaCovid(@RequestBody Covid covid) {
 	    return	covidRepository.save(covid);
 	}
+	
+
 }
