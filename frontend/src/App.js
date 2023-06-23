918329895b09d9dc8d0412befdb6179c62b0c4e0
Hussyvel Ribeiro <hussyribeiro@hotmail.com>
2023-06-23 01:09:37 -0300

    correcao de bug do delete e atualizacao

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
-
-import { render } from "@testing-library/react";
+import { render } from "@testing-library/react"; 
 import React, {Component} from "react";
 import Header from './components/Header';
 
